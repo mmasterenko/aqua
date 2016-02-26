42849e717e30b76b87d6c2268e578fde8d87e7f7
Maksim Masterenko <mmasterenko@gmail.com>
2016-02-26 18:55:11 +0300

    model admin progress

--- catalogApp/admin.py
+++ catalogApp/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from .models import Goods, Attributes, Catalog
+
+
+class AttributesInline(admin.TabularInline):
+    model = Attributes
+    extra = 0
+    can_delete = True
+
+
+class GoodsAdmin(admin.ModelAdmin):
+    inlines = [AttributesInline]
+    search_fields = ('name', 'catalog__name')
+    list_display = ('name', 'price', 'catalog')
+
+
+class AttributesAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'value', 'goods__name')
+    list_display = ('__unicode__', 'goods')
+
+
+admin.site.register(Catalog)
+admin.site.register(Goods, GoodsAdmin)
+admin.site.register(Attributes, AttributesAdmin)
+
+admin.site.site_header = u'Интерфейс администратора'
+admin.site.index_title = u'Управление'
--- catalogApp/models.py
+++ catalogApp/models.py
@@ -1,3 +1,44 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+
+class Catalog(models.Model):
+    class Meta:
+        verbose_name_plural = u'каталог'
+        verbose_name = u'каталог'
+
+    def __unicode__(self):
+        return self.name
+
+    name = models.CharField(u'название каталога', max_length=90, unique=True)
+
+
+class Goods(models.Model):
+    class Meta:
+        verbose_name = u'товар'
+        verbose_name_plural = u'товары'
+        unique_together = ('name', 'catalog')
+
+    def __unicode__(self):
+        return self.name
+
+    catalog = models.ForeignKey(Catalog, verbose_name=u'каталог', null=True, blank=True)
+
+    name = models.CharField(u'название', max_length=90)
+    price = models.DecimalField(u'цена', max_digits=9, decimal_places=2, null=True, blank=True)
+
+
+class Attributes(models.Model):
+    class Meta:
+        verbose_name = u'аттрибут'
+        verbose_name_plural = u'аттрибуты'
+        unique_together = ('name', 'goods')
+
+    def __unicode__(self):
+        return u'%s:%s' % (self.name, self.value)
+
+    goods = models.ForeignKey(Goods, verbose_name=u'товар', null=True, blank=True)
+
+    name = models.CharField(u'имя', max_length=90)
+    value = models.CharField(u'значение', max_length=90, null=True, blank=True)
