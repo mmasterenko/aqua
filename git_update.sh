328b62c75e825b2f3e3662608e9cb03b2afdb911
Maksim Masterenko <mmasterenko@gmail.com>
2016-02-26 19:42:24 +0300

    git_update.sh & gunicorn.sh

---
+++ git_update.sh
@@ -0,0 +1,7 @@
+git pull
+source ./venv/bin/activate
+./venv/bin/python manage.py makemigrations
+./venv/bin/python manage.py migrate
+yes yes | ./venv/bin/python manage.py collectstatic
+kill -HUP `pgrep gunicorn`
+
