caa837001f90fc15adeb97ce9aaa90fa74ba0749
Maksim Masterenko <mmasterenko@gmail.com>
2016-02-26 20:16:04 +0300

    specify which gunicorn killHUP

--- git_update.sh
+++ git_update.sh
@@ -3,5 +3,5 @@ source ./venv/bin/activate
 ./venv/bin/python manage.py makemigrations
 ./venv/bin/python manage.py migrate
 yes yes | ./venv/bin/python manage.py collectstatic
-kill -HUP `pgrep gunicorn`
+kill -HUP `pgrep -f 'gunicorn aqua'`
 
