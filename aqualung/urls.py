64957e549a670870912bbd47bfef4a496c342d97
Maksim Masterenko <mmasterenko@gmail.com>
2016-02-26 21:51:16 +0300

    media: url & view

--- aqualung/urls.py
+++ aqualung/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from .utils import extra_views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'aqualung.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    # on production server it's better to use nginx for serve static
+    url(r'^media/(?P<path>images/.+(?:\.jpeg|\.jpg|\.png))$', extra_views.media, name='media'),
+    url(r'^media/(?P<path>files/.+(?:\.pdf|\.doc|\.docx|\.txt))$', extra_views.media, name='media'),
 
     url(r'^aquadmin/', include(admin.site.urls)),
 ]
