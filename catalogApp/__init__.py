02ee3bb748eda69b3f77fbc54137a46cf0e25209
Maksim Masterenko <mmasterenko@gmail.com>
2016-02-26 18:55:55 +0300

    catalogApp verbose_name

---
+++ catalogApp/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'catalogApp.config.CatalogAppConfig'
---
+++ catalogApp/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class CatalogAppConfig(AppConfig):
+    name = 'catalogApp'
+    verbose_name = u'Каталог'
+
