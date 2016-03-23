6870171c66b89ec2374427803388a3d1b47e7a5e
Maksim Masterenko <mmasterenko@gmail.com>
2016-03-23 22:41:14 +0300

    catalog view & template

--- catalogApp/views.py
+++ catalogApp/views.py
@@ -1,6 +1,11 @@
+from utils import group_list
 from django.shortcuts import render
 from .models import Catalog
 
 
 def categories(req):
-    pass
+    context = {
+        'category_rows': group_list(Catalog.objects.all(), 3),
+        'title': 'Aqua'
+    }
+    return render(req, 'catalogApp/portfolio-3-col.html', context=context)
