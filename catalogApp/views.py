4c5c63e65ed6f6e0f840a4bc8b0b6f90e3f7499b
Maksim Masterenko <mmasterenko@gmail.com>
2016-03-23 23:38:29 +0300

    catalog page

--- catalogApp/views.py
+++ catalogApp/views.py
@@ -1,11 +1,21 @@
+# -*- coding: utf-8 -*-
+
 from utils import group_list
 from django.shortcuts import render
 from .models import Catalog
 
 
 def categories(req):
-    context = {
-        'category_rows': group_list(Catalog.objects.all(), 3),
-        'title': 'Aqua'
+    general = {
+        'keywords': u'водолазное снаряжение',
+        'description': u'водолазное снаряжение',
+        'title': 'Aqua',
+        'heading': u'Каталог',
+        'subheading': u'',
+        'breadcrumbs': [dict(name=u'каталог', url='#')]
     }
+    context = {
+        'category_rows': group_list(Catalog.objects.all(), 3)
+    }
+    context.update(general)
     return render(req, 'catalogApp/portfolio-3-col.html', context=context)
