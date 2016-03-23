0ed76646497e0cdc44c10822b292e99162334318
Maksim Masterenko <mmasterenko@gmail.com>
2016-03-23 22:37:58 +0300

    home url /

--- aqualung/urls.py
+++ aqualung/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .utils import extra_views
+from utils import extra_views
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'aqualung.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'catalogApp.views.categories', name='categories'),
 
     # on production server it's better to use nginx for serve static
     url(r'^media/(?P<path>images/.+(?:\.jpeg|\.jpg|\.png))$', extra_views.media, name='media'),
