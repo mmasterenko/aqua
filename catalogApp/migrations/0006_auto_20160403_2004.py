badeac8d3636116f98b0ff8d8101ef98bfbdb9e6
Maksim Masterenko <mmasterenko@gmail.com>
2016-04-03 20:06:24 +0300

    new migrations

---
+++ catalogApp/migrations/0006_auto_20160403_2004.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catalogApp', '0005_generalinfo'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='generalinfo',
+            options={'verbose_name': '\u041e\u0431\u0449\u0430\u044f \u0438\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u044f', 'verbose_name_plural': '\u041e\u0431\u0449\u0430\u044f \u0438\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u044f'},
+        ),
+    ]
