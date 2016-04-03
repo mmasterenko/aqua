d04232ec7b95887a779c577b3cfe8b7da27b1d84
Maksim Masterenko <mmasterenko@gmail.com>
2016-04-03 13:33:54 +0300

    catalog, catalogItem, goods

--- aqualung/urls.py
+++ aqualung/urls.py
@@ -5,7 +5,10 @@ from utils import extra_views
 urlpatterns = [
     # Examples:
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'catalogApp.views.categories', name='categories'),
+    url(r'^$', 'catalogApp.views.categories', name='home'),
+    url(r'^catalog$', 'catalogApp.views.categories', name='catalog'),
+    url(r'^catalog/(?P<catalog_id>[0-9]+)', 'catalogApp.views.catalog', name='catalog_item'),
+    url(r'^goods/(?P<good_id>[0-9]+)', 'catalogApp.views.good_item', name='good_item'),
 
     # on production server it's better to use nginx for serve static
     url(r'^media/(?P<path>images/.+(?:\.jpeg|\.jpg|\.png))$', extra_views.media, name='media'),
--- catalogApp/views.py
+++ catalogApp/views.py
@@ -2,21 +2,36 @@
 
 from utils import group_list
 from django.shortcuts import render
-from .models import Catalog
+from django.http import HttpResponse
+from .models import Catalog, Goods
+
+general = {
+    'keywords': u'водолазное снаряжение',
+    'description': u'водолазное снаряжение',
+    'title': u'Водолазное оборудование',
+    'brandname': u'Моя контора',
+    'heading': u'Каталог',
+    'subheading': u'',
+    'breadcrumbs': [dict(name=u'каталог', url='#')]
+}
 
 
 def categories(req):
-    general = {
-        'keywords': u'водолазное снаряжение',
-        'description': u'водолазное снаряжение',
-        'title': u'Водолазное оборудование',
-        'brandname': u'Моя контора',
-        'heading': u'Каталог',
-        'subheading': u'',
-        'breadcrumbs': [dict(name=u'каталог', url='#')]
-    }
     context = {
         'category_rows': group_list(Catalog.objects.all(), 3)
     }
     context.update(general)
     return render(req, 'catalogApp/catalog.html', context=context)
+
+
+def catalog(req, catalog_id=None):
+    context = {'goods': Goods.objects.filter(catalog_id=catalog_id)}
+    context.update(general)
+    return render(req, 'catalogApp/catalog_item.html', context=context)
+
+
+def good_item(req, good_id=None):
+    context = {'good_item': Goods.objects.get(pk=good_id)}
+    context.update(general)
+    return render(req, 'catalogApp/good_item.html', context=context)
+
